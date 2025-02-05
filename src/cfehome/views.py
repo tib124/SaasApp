b6d5d149273dd72d7946a91f58164a6b3279c076
Thiago Oliveira <thiago_9_oliveira@hotmail.com>
2025-02-05 16:43:21 -0300

    Fix a bug where percent wasnt working

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -5,26 +5,31 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def about_view(request,*args,**kwargs):
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
-    try: 
+    page_qs = PageVisit.objects.filter(path=request.path)  # Definir antes de usar
+
+    try:
         percent = (page_qs.count() * 100.0) / qs.count()
-    except:
-        percent = 0
-        
-    page_qs = PageVisit.objects.filter(path=request.path)
+    except ZeroDivisionError:
+        percent = 0  # Evita divisão por zero
+
     my_title = "My page"
     my_context = {
         "page_title": my_title,
-        "page_visit_count" : page_qs.count(),
-        "Percent" : percent,
-        "total_visit_count" : qs.count()
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
     }
+    
     path = request.path
-    print("path",path)
+    print("path", path)
+    
     html_template = "home.html"
     PageVisit.objects.create(path=request.path)
-    return render(request,html_template,my_context)
+    
+    return render(request, html_template, my_context)
+
 
 
 def home_view(request,*args,**kwargs):
